6642ef344e4c326a8791098ce8f4173e7faa26dc
newaltcoin <newaltcoin@gmail.com>
2015-06-28 22:58:40 +0300

    Update moneta-cli.cpp

--- src/moneta-cli.cpp
+++ src/moneta-cli.cpp
@@ -67,7 +67,7 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc<2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("Litcoin Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Moneta Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
                   "  moneta-cli [options] <command> [params]  " + _("Send command to Moneta Core") + "\n" +
